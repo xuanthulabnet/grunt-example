437c0a2e849aad881f2b7516d3d3010c6ca5a7c4
XTLAB <xtl@xuanthulab.net>
2018-12-02 15:55:37 +0700

    U

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,6 @@
-
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
- 
-          
         uglify: {
             options: {
                 banner: '/*! Dòng này chèn vào đầu file <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -11,60 +8,15 @@ module.exports = function(grunt) {
     
             build: {
                 files: {
-                    'build/hello.min.js' : ['tmp/all_back.js']
+                    'build/hello.min.js' : ['src/hello.js']
                 }
-            }, 
-        },
-        concat: {
-          build: {
-            files: {
-                'tmp/all.js' : ['src/hello.js', 'src/ab.js']
-            }  
-          }  
-        },
-
-        babel: {
-            options: {
-                presets: ['@babel/preset-env']
-              },
-              build: {
-                files: {
-                  'tmp/all_back.js': 'tmp/all.js'
-                }
-              }
-          
-        },
-
-        jshint: {
-            options: {
-                '-W015': true,
-                "esversion": 6
-              },        
-            build: {
-              src: ['tmp/all.js']
-            },
-          },
-
-       clean: {
-        build: {
-            src: ['tmp/*']
-          }, 
-       }   
-
+            }
+        }
     });
 
-  grunt.loadNpmTasks('grunt-babel');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default', [
-                                 'concat:build', 
-                                 'jshint:build', 
-                                 'babel:build', 
-                                 'uglify:build',
-                                 'clean'   
-                                ]);
- 
+
+  grunt.registerTask('default', ['uglify']);
+
 }
